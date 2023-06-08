a64ecac638c3ea68ec58e74cf59d741180fb97d7
Tien Nguyen Minh <tien.nguyenminh@nashtechglobal.com>
2023-06-08 11:24:37 +0700

    format

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,9 +1,9 @@
 variable "Environment" {
-  type = string
-	default = "changeme"
+  type    = string
+  default = "changeme"
 }
 
 variable "Component" {
-  type = string
-	default = "changeme"
+  type    = string
+  default = "changeme"
 }
