729da8cfc80ca3bd7d81ffaccc3208b138945a4c
Tien Nguyen Minh <tien.nguyenminh@nashtechglobal.com>
2023-06-08 11:21:54 +0700

    update reference and try

--- terraform/s3_bucket.tf
+++ terraform/s3_bucket.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "backend" {
-  bucket = "${Component}-${Environment}-backend-bucket"
+  bucket = "${var.Component}-${var.Environment}-backend-bucket"
   tags = {
-    Component   = "${Component}"
-    Environment = "${Environment}"
+    Component   = "${var.Component}"
+    Environment = "${var.Environment}"
   }
 }
